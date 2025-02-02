f168f0e1de46fc8d89abdbe463f2dd8b71014309
janmerjacamo <160087981+janmerjacamo@users.noreply.github.com>
2025-02-01 22:19:21 -0600

    [ADD] files

--- fel_infile/__manifest__.py
+++ fel_infile/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- encoding: utf-8 -*-
 
 {
-    'name': 'FEL Infile XIM',
+    'name': 'FEL Infile',
     'version': '1.1',
     'category': 'Custom',
     'description': """ Integración con factura electrónica de Infile """,
-    'author': 'Janmer Jacamo ',
-    'website': 'www.xim.technology/',
+    'author': 'aquíH',
+    'website': 'http://xim.technology/',
     'depends': ['fel_gt'],
     'data': [
         'views/account_view.xml',
@@ -16,4 +16,3 @@
     'installable': True
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
